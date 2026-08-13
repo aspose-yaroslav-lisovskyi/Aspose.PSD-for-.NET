b0b42e9c5268bab6ab00e72d4955e22c1f693024
Yaroslav Lisovsky <yaroslav.lisovskyi@aspose.com>
2026-08-13 10:13:50 +0300

    Update AssemblyCopyright

--- Examples/CSharp/Properties/AssemblyInfo.cs
+++ Examples/CSharp/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © Aspose 2018-2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
